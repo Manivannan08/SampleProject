9e1dcd7159d7e738e3e4f3e6bc607a5c4d787905
Manivannan08 <manivannantvm0@gmail.com>
2021-10-21 17:22:20 +0530

    Ravi update

--- src/test/java/org/sample/A.java
+++ src/test/java/org/sample/A.java
@@ -2,7 +2,7 @@ package org.sample;
 
 public class A {
  public static void main(String[] args) {
-	System.out.println("A1");
-	System.out.println("A2");
+	System.out.println("A3");
+	System.out.println("A4");
 }
 }
